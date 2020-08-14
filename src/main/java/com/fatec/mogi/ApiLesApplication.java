4ca85765a1261d5c6cab0e879d9e444f9745594f
Matheus Wendel <matheus_wendel@outlook.com>
2020-08-14 01:47:40 -0300

    Removed auto configuration of spring security

--- src/main/java/com/fatec/mogi/ApiLesApplication.java
+++ src/main/java/com/fatec/mogi/ApiLesApplication.java
@@ -2,8 +2,9 @@ package com.fatec.mogi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApiLesApplication {
 
 	public static void main(String[] args) {
