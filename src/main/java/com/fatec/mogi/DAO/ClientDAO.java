0f65810c414aa02032bb7cf04cba6d5b5fa29862
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-03 23:57:14 -0300

    Removed inused imports

--- src/main/java/com/fatec/mogi/DAO/ClientDAO.java
+++ src/main/java/com/fatec/mogi/DAO/ClientDAO.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-import com.fatec.mogi.model.aplication.Filter;
-import com.fatec.mogi.model.aplication.Result;
 import com.fatec.mogi.model.domain.Client;
-import com.fatec.mogi.model.domain.DomainEntity;
 @Service
 public class ClientDAO extends AbstractDAO<Client> {
 
