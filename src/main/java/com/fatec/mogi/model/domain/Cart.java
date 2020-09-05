dce0cbf090896e5701af35bd5ea7ac9888209c3f
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-05 10:44:03 -0300

    Removed bilateral one to one mapping with client

--- src/main/java/com/fatec/mogi/model/domain/Cart.java
+++ src/main/java/com/fatec/mogi/model/domain/Cart.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Cart extends DomainEntity {
 	@OneToMany
 	private List<CartProduct> CartProducts;
-	@OneToOne
-	private Client client;
+	
 }
