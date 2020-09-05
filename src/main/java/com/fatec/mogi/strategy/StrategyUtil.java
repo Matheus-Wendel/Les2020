079efa5a2641705529d2310393adb03884e784bb
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-05 11:21:00 -0300

    Fixed strategy construction

--- src/main/java/com/fatec/mogi/strategy/StrategyUtil.java
+++ src/main/java/com/fatec/mogi/strategy/StrategyUtil.java
@@ -5,35 +5,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.mogi.repository.CityRepository;
+import com.fatec.mogi.repository.ClientRepository;
 import com.fatec.mogi.util.CrudOperationEnum;
+
 @Service
 public class StrategyUtil {
 
-//	@Autowired
-//	UserRepository userRepository;
-	
-	
-	
+	@Autowired
+	ClientRepository clientRepository;
+
+	@Autowired
+	CityRepository cityRepository;
+
 	public Map<String, List<IStrategy>> getStrategies() {
-		//Strategies Lists
-		List<IStrategy> clientValidations =new ArrayList<>();
-		
-		//Strategies Instances
-		ClientValidation clientValidation = new ClientValidation();
-		
-		//Filling the lists		
+		// Strategies Lists
+		List<IStrategy> clientValidations = new ArrayList<>();
+
+		// Strategies Instances
+		AddressValidation addressValidation = new AddressValidation(cityRepository);
+		ClientValidation clientValidation = new ClientValidation(clientRepository, addressValidation);
+
+		// Filling the lists
 		clientValidations.add(clientValidation);
-		
-		//Strategy map
+
+		// Strategy map
 		Map<String, List<IStrategy>> strategiesMap = new HashMap<>();
-		
-		//Filling the map
-		strategiesMap.put("client"+CrudOperationEnum.SAVE.name(), clientValidations);
-		
+
+		// Filling the map
+		strategiesMap.put("client" + CrudOperationEnum.SAVE.name(), clientValidations);
+
 		return strategiesMap;
 	}
-	
-	
+
 }
