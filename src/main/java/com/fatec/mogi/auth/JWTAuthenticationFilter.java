1611b2d3a05c867be394e576c2653c406deaeda9
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-05 20:02:46 -0300

    Added login by jwt

---
+++ src/main/java/com/fatec/mogi/auth/JWTAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.fatec.mogi.auth;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+	
+	 @Override
+	    public Authentication attemptAuthentication(HttpServletRequest req,
+	                                                HttpServletResponse res) throws AuthenticationException {
+	        try {
+	        	 com.fatec.mogi.model.domain.User creds = new ObjectMapper()
+	                    .readValue(req.getInputStream(),  com.fatec.mogi.model.domain.User.class);
+
+	            return authenticationManager.authenticate(
+	                    new UsernamePasswordAuthenticationToken(
+	                            creds.getEmail(),
+	                            creds.getPassword(),
+	                            new ArrayList<>())
+	            );
+	        } catch (IOException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+
+	    @Override
+	    protected void successfulAuthentication(HttpServletRequest req,
+	                                            HttpServletResponse res,
+	                                            FilterChain chain,
+	                                            Authentication auth) throws IOException, ServletException {
+
+//	        String token = JWT.create()
+//	                .withSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername())
+//	                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+//	                .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+//	        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+//	        
+//	        PrintWriter writer = res.getWriter();
+//	        res.setContentType("application/json");
+//	        res.setCharacterEncoding("UTF-8");
+//	        
+//	        String body ="\""+ SecurityConstants.TOKEN_PREFIX + token+"\"";
+//			writer.print(body.toString());
+//	        writer.flush();
+	    	Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+	    	for (GrantedAuthority grantedAuthority : authorities) {
+				System.err.println( grantedAuthority.getAuthority());
+			}
+	    	 String token = JWT.create()
+		                .withSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername())
+		                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+		                .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+		        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+		        res.addHeader(SecurityConstants.PERMISSION_STRING, auth.getAuthorities().iterator().next().toString());
+
+	    }
+}
---
+++ src/main/java/com/fatec/mogi/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.fatec.mogi.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fatec.mogi.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		com.fatec.mogi.model.domain.User user = userRepository.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				getAuthorities(user));
+	}
+
+	private static Collection<? extends GrantedAuthority> getAuthorities(com.fatec.mogi.model.domain.User user) {
+
+		// CRIA UMA LISTA DE PERMISSOES
+		// A QUAL SERA A ATRIBUIDA A ELA AS PERMICOES QUE OS NOMES ESTAO SALVOS NO
+		// PROPRIO USUARIO( usuario.getPermissao() )
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority(user.getPermission().toString()));
+
+		return authorities;
+	}
+
+}
