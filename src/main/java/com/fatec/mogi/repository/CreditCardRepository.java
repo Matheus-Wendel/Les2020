f3a35c54d4c48aa703980b163492152bc939319a
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-13 03:13:00 -0300

    Credit card repo

---
+++ src/main/java/com/fatec/mogi/repository/CreditCardRepository.java
@@ -0,0 +1,10 @@
+package com.fatec.mogi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mogi.model.domain.Address;
+import com.fatec.mogi.model.domain.CreditCard;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard, Integer> {
+
+}
