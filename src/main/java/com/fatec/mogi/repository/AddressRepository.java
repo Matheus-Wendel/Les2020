31130cde2838ce86099aa839e4e4a5a029986302
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-13 03:12:26 -0300

    Address repo

---
+++ src/main/java/com/fatec/mogi/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.fatec.mogi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mogi.model.domain.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+	
+	
+}
