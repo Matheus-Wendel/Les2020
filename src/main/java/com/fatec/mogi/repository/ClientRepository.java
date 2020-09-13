48aad79a067d25dfd9eb0527bad49f68cdfd99f1
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-13 19:03:22 -0300

    Deactivation query

--- src/main/java/com/fatec/mogi/repository/ClientRepository.java
+++ src/main/java/com/fatec/mogi/repository/ClientRepository.java
@@ -1,6 +1,9 @@
 package com.fatec.mogi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fatec.mogi.model.domain.Client;
 
@@ -8,4 +11,8 @@ public interface ClientRepository extends JpaRepository<Client, Integer> {
 
 
 	public Client findByUserEmail(String email);
+	
+	@Modifying(clearAutomatically = true)
+	@Query("update Client c set c.active=0 where c.id= :id")
+	public void deactivate(@Param(value = "id") Integer id); 
 }
