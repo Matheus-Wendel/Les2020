588016861ba5c356f0e8686c0a5e3c6012ae15b8
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-13 18:56:01 -0300

    Changed delete to deactivation of client

--- src/main/java/com/fatec/mogi/DAO/ClientDAO.java
+++ src/main/java/com/fatec/mogi/DAO/ClientDAO.java
@@ -1,31 +1,62 @@
 package com.fatec.mogi.DAO;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import com.fatec.mogi.auth.AuthUtils;
 import com.fatec.mogi.model.aplication.Filter;
 import com.fatec.mogi.model.aplication.Result;
 import com.fatec.mogi.model.domain.Client;
 import com.fatec.mogi.model.domain.DomainEntity;
 import com.fatec.mogi.repository.AddressRepository;
+import com.fatec.mogi.repository.ClientRepository;
+import com.fatec.mogi.util.MessagesUtil;
+
 @Service
 public class ClientDAO extends AbstractDAO<Client> {
 
 	@Autowired
 	AddressRepository addressRepository;
 	
+	ClientRepository clientRepository;
+
 	@Autowired
 	public ClientDAO(JpaRepository<Client, Integer> repository) {
 		super(repository);
+		this.clientRepository = (ClientRepository) repository;
 	}
-	
-	
-	
+
 	@Override
 	public Result save(Filter<? extends DomainEntity> filter) {
-		var client = (Client)filter.getEntity(); 
-		client.getDeliveryAddresses().stream().forEach(a-> a.setClient(client));
+		var client = (Client) filter.getEntity();
+		client.getDeliveryAddresses().stream().forEach(a -> a.setClient(client));
 		return super.save(filter);
 	}
+
+	@Override
+
+	@Transactional
+	public Result delete(Filter<? extends DomainEntity> filter) {
+		Client entity = AuthUtils.getLoggedClient();
+		Result result = new Result();
+		try {
+			if (repository.existsById(entity.getId())) {
+				this.clientRepository.deactivate(entity.getId());
+				result.getMessages().put(MessagesUtil.DEACTIVATED, "id: " + entity.getId());
+
+			} else {
+				result.getMessages().put(MessagesUtil.NOT_FOUND, "id: " + entity.getId());
+			}
+			return result;
+		} catch (Exception e) {
+			result.setError(true);
+			result.getMessages().put("Message", e.getMessage());
+			result.getMessages().put("Cause", e.getCause().toString());
+			return result;
+		}
+	}
+
 }
