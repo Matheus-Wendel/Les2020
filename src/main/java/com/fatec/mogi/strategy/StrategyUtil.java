3f9664ccf57fca914f20383eb37d61e7608cd96b
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-13 19:04:51 -0300

    Added credidcard validation to the map

--- src/main/java/com/fatec/mogi/strategy/StrategyUtil.java
+++ src/main/java/com/fatec/mogi/strategy/StrategyUtil.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.mogi.repository.CardBrandRepository;
 import com.fatec.mogi.repository.CityRepository;
 import com.fatec.mogi.repository.ClientRepository;
 import com.fatec.mogi.util.CrudOperationEnum;
@@ -20,19 +21,23 @@ public class StrategyUtil {
 
 	@Autowired
 	CityRepository cityRepository;
+	@Autowired
+	CardBrandRepository cardBrandRepository;
 
 	public Map<String, List<IStrategy>> getStrategies() {
 		// Strategies Lists
 		List<IStrategy> clientValidations = new ArrayList<>();
 		List<IStrategy> addressValidations = new ArrayList<>();
+		List<IStrategy> creditCardValidations = new ArrayList<>();
 
 		// Strategies Instances
 		AddressValidation addressValidation = new AddressValidation(cityRepository);
 		ClientValidation clientValidation = new ClientValidation(clientRepository, addressValidation);
-
+		CreditCardValidation creditCardValidation = new CreditCardValidation(cardBrandRepository);
 		// Filling the lists
 		clientValidations.add(clientValidation);
 		addressValidations.add(addressValidation);
+		creditCardValidations.add(creditCardValidation);
 
 		// Strategy map
 		Map<String, List<IStrategy>> strategiesMap = new HashMap<>();
@@ -41,6 +46,7 @@ public class StrategyUtil {
 		strategiesMap.put("client" + CrudOperationEnum.SAVE.name(), clientValidations);
 		strategiesMap.put("address" + CrudOperationEnum.SAVE.name(), addressValidations);
 		strategiesMap.put("address" + CrudOperationEnum.UPDATE.name(), addressValidations);
+		strategiesMap.put("creditcard" + CrudOperationEnum.SAVE.name(), creditCardValidations);
 
 		return strategiesMap;
 	}
