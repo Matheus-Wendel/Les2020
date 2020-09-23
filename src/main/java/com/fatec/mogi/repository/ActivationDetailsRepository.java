cbdb2012f42f15e8361d7312faabbad06da1ddc4
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-23 02:00:43 -0300

    added ActivationDetails Repository

---
+++ src/main/java/com/fatec/mogi/repository/ActivationDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.fatec.mogi.repository;
+
+import com.fatec.mogi.model.domain.ActivationDetails;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivationDetailsRepository extends JpaRepository<ActivationDetails, Integer> {
+
+	
+	
+}
