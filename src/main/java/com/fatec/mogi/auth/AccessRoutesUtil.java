74e8e560e5e9e3ab80c071c4bfa6489affe17407
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-24 20:12:07 -0300

    Adicionada rotas exclusivas para cliente

--- src/main/java/com/fatec/mogi/auth/AccessRoutesUtil.java
+++ src/main/java/com/fatec/mogi/auth/AccessRoutesUtil.java
@@ -6,21 +6,23 @@ public class AccessRoutesUtil {
 	"/disc",
 	"/artist",
 	"/genre",
-	"/recoder",
+	"/recorder",
 	"/cardBrand",
 	"/city",
-	"/client",
 	"/country",
-	"/creditCard",
 	"/state" };
 
 	//client only
-	public static final String[] POST_CLIENT_ACCESS_ROUTES = {"/address","/creditCard"};
+	public static final String[] GET_CLIENT_ACCESS_ROUTES = {"/cart","/cartProduct"};
+	public static final String[] POST_CLIENT_ACCESS_ROUTES = {"/address","/creditCard","/cart","/cartProduct"};
+	public static final String[] DELETE_CLIENT_ACCESS_ROUTES = {"/address","/creditCard","/client"};
+	
 	public static final String[] PUT_CLIENT_ACCESS_ROUTES = { "/client", "/address", "/creditCard"};
 	
 	
 	//employee only
 	public static final String[] POST_EMPLOYEE_ACCESS_ROUTES = { "/disc", "/artist", "/genre","/recorder","/pricing","/stock","/sale","/employee"};
 	public static final String[] PUT_EMPLOYEE_ACCESS_ROUTES = { "/disc", "/artist", "/genre","/recorder","/pricing","/stock","/sale","/employee"};
+	public static final String[] DELETE_EMPLOYEE_ACCESS_ROUTES = { "/disc", "/artist", "/genre","/recorder","/pricing","/stock","/sale"};
 
 }
