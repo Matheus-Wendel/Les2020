ac7d73a194513b4dc92210e99015df62b1e4a501
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-26 15:59:37 -0300

    enum for purchase item status

--- src/main/java/com/fatec/mogi/model/domain/PurchaseItem.java
+++ src/main/java/com/fatec/mogi/model/domain/PurchaseItem.java
@@ -2,24 +2,39 @@ package com.fatec.mogi.model.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
+
+import com.fatec.mogi.enumeration.PurchaseItemEnum;
 
 @Entity
 public class PurchaseItem extends DomainEntity {
 	@Column(nullable = false)
-	private String status;
+	@Enumerated(EnumType.STRING)
+	private PurchaseItemEnum status;
 	@ManyToOne
 	private Disc disc;
 
-	public String getStatus() {
+	public PurchaseItem() {
+	}
+	public PurchaseItem(PurchaseItemEnum status, Disc disc) {
+		this.status = status;
+		this.disc = disc;
+	}
+
+	public PurchaseItemEnum getStatus() {
 		return status;
 	}
-	public void setStatus(String status) {
+
+	public void setStatus(PurchaseItemEnum status) {
 		this.status = status;
 	}
+
 	public Disc getDisc() {
 		return disc;
 	}
+
 	public void setDisc(Disc disc) {
 		this.disc = disc;
 	}
