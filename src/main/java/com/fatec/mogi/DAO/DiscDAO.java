b5b55f128775a5c9fcf86ab5e39652b0af89dcf9
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-27 23:42:43 -0300

    find by discName

--- src/main/java/com/fatec/mogi/DAO/DiscDAO.java
+++ src/main/java/com/fatec/mogi/DAO/DiscDAO.java
@@ -11,6 +11,7 @@ import com.fatec.mogi.model.aplication.Result;
 import com.fatec.mogi.model.domain.Disc;
 import com.fatec.mogi.model.domain.DomainEntity;
 import com.fatec.mogi.repository.ActivationDetailsRepository;
+import com.fatec.mogi.repository.DiscRepository;
 import com.fatec.mogi.repository.StockRepository;
 
 @Service
@@ -22,9 +23,12 @@ public class DiscDAO extends AbstractDAO<Disc> {
 	@Autowired
 	ActivationDetailsRepository activationDetailsRepository;
 
+	DiscRepository discRepository;
+
 	@Autowired
 	public DiscDAO(JpaRepository<Disc, Integer> repository) {
 		super(repository);
+		this.discRepository = (DiscRepository) repository;
 	}
 
 	@Override
@@ -35,6 +39,17 @@ public class DiscDAO extends AbstractDAO<Disc> {
 		stockRepository.saveAll(disc.getStock());
 		activationDetailsRepository.save(disc.getActivationDetails());
 		return super.save(filter);
+	}
+
+	@Override
+	public Result find(Filter<? extends DomainEntity> filter) {
+		var disc = (Disc) filter.getEntity();
+		if (disc != null && disc.getName() != null && !disc.getName().isBlank()) {
+			Result result = new Result();
+			result.setResultList(discRepository.findByNameContaining(disc.getName()));
+			return result;
+		}
+		return super.find(filter);
 	}
 
 }
--- src/main/java/com/fatec/mogi/repository/DiscRepository.java
+++ src/main/java/com/fatec/mogi/repository/DiscRepository.java
@@ -1,5 +1,7 @@
 package com.fatec.mogi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fatec.mogi.model.domain.Disc;
@@ -7,5 +9,5 @@ import com.fatec.mogi.model.domain.Disc;
 public interface DiscRepository extends JpaRepository<Disc, Integer> {
 
 	
-	
+	List<Disc> findByNameContaining(String name);
 }
