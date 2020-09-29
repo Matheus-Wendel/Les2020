911fc226f33916cdc101465112325906586215a0
Matheus Wendel <matheus_wendel@outlook.com>
2020-09-28 21:56:19 -0300

    find by disc name

--- src/main/java/com/fatec/mogi/controller/DiscController.java
+++ src/main/java/com/fatec/mogi/controller/DiscController.java
@@ -1,9 +1,13 @@
 package com.fatec.mogi.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.mogi.model.aplication.Filter;
 import com.fatec.mogi.model.domain.Disc;
 
 @RestController
@@ -14,6 +18,16 @@ public class DiscController extends AbstractController<Disc> {
 	public DiscController() {
 		super(Disc.class);
 
+	}
+
+	@SuppressWarnings("rawtypes")
+	@GetMapping(path = "/{name}")
+	public ResponseEntity find( @PathVariable(name = "name") String name) {
+		System.err.println(name);
+		var disc = new Disc();
+		disc.setName(name);
+		Filter<Disc> filter =new Filter<Disc>(disc,this.clazz);
+		return findCommand.execute(filter).buildResponse();
 	}
 
 }
