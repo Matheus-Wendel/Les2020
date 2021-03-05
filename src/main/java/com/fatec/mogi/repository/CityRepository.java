65d6b7cacda4a6ad78e39113d5d663456ebe4ace
Matheus Wendel <matheus_wendel@outlook.com>
2021-03-05 00:03:49 -0300

    Busca estado por id

--- src/main/java/com/fatec/mogi/repository/CityRepository.java
+++ src/main/java/com/fatec/mogi/repository/CityRepository.java
@@ -1,11 +1,15 @@
 package com.fatec.mogi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fatec.mogi.model.domain.City;
 
 public interface CityRepository extends JpaRepository<City, Integer> {
 
+	
+	List<City> findByStateId(Integer id);
 	
 	
 }
