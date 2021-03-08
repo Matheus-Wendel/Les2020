42276f250fc4dbc562f9d8508c0ad7f7084057b8
Matheus Wendel <matheus_wendel@outlook.com>
2021-03-08 03:30:56 -0300

    Busca todos o endereços do cliente ao inves de tazer pelo cliente logado

--- src/main/java/com/fatec/mogi/DAO/AddressDAO.java
+++ src/main/java/com/fatec/mogi/DAO/AddressDAO.java
@@ -9,14 +9,19 @@ import com.fatec.mogi.enumeration.PermissionEnum;
 import com.fatec.mogi.model.aplication.Filter;
 import com.fatec.mogi.model.aplication.Result;
 import com.fatec.mogi.model.domain.Address;
+import com.fatec.mogi.model.domain.Client;
 import com.fatec.mogi.model.domain.DomainEntity;
+import com.fatec.mogi.repository.AddressRepository;
 
 @Service
 public class AddressDAO extends AbstractDAO<Address> {
 
+	AddressRepository addressRepository;
+	
 	@Autowired
 	public AddressDAO(JpaRepository<Address, Integer> repository) {
 		super(repository);
+		this.addressRepository = (AddressRepository) repository;
 	}
 
 	@Override
@@ -33,7 +38,9 @@ public class AddressDAO extends AbstractDAO<Address> {
 			return super.find(filter);
 		}
 		Result result = new Result();
-		result.setResultList((AuthUtils.getLoggedClient().getDeliveryAddresses()));
+		Client loggedClient = AuthUtils.getLoggedClient();
+		Integer id = loggedClient.getId();
+		result.setResultList(this.addressRepository.findByClientId(id));
 		return result;
 	}
 }
