3c21fa83093d355bd1c68711247d7dc0a6191d6c
Matheus Wendel <matheus_wendel@outlook.com>
2021-03-08 03:30:19 -0300

    removido temporiariamente

--- src/main/java/com/fatec/mogi/controller/CartProductController.java
+++ src/main/java/com/fatec/mogi/controller/CartProductController.java
@@ -1,10 +1,7 @@
 package com.fatec.mogi.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.mogi.model.domain.CartProduct;
@@ -19,12 +16,5 @@ public class CartProductController extends AbstractController<CartProduct> {
 
 	}
 
-	@DeleteMapping
-	public ResponseEntity delete(@RequestParam Integer id) {
-		var cartProduct = new CartProduct();
-		cartProduct.setId(id);
-
-		return super.delete(cartProduct);
-	}
 
 }
