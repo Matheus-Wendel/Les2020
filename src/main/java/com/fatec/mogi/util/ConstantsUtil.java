48ff8ac1f10444ea1477af1a35c019a864dd9c52
Matheus Wendel <matheus_wendel@outlook.com>
2021-03-08 03:32:52 -0300

    Formato da data de conversão igual a input html do tipo month

--- src/main/java/com/fatec/mogi/util/ConstantsUtil.java
+++ src/main/java/com/fatec/mogi/util/ConstantsUtil.java
@@ -2,6 +2,6 @@ package com.fatec.mogi.util;
 
 public class ConstantsUtil {
 	public static final String DATE_FORMAT = "yyyy-MM-dd";
-	public static final String CREDIT_CARD_EXPIRATION_DATE_FORMAT= "MM-yyyy";
+	public static final String CREDIT_CARD_EXPIRATION_DATE_FORMAT= "yyyy-MM";
 	public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd-HH:mm";
 }
