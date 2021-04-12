7a63f1ba3945a91285a5f77dfdf2778a0715d7fd
Matheus Wendel <matheus_wendel@outlook.com>
2021-04-12 17:36:15 -0300

    Rota que recebe um disco e a adiciona ao carrinho do cliente

--- src/main/java/com/fatec/mogi/controller/CartProductController.java
+++ src/main/java/com/fatec/mogi/controller/CartProductController.java
@@ -1,10 +1,15 @@
 package com.fatec.mogi.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.mogi.model.aplication.Filter;
 import com.fatec.mogi.model.domain.CartProduct;
+import com.fatec.mogi.model.domain.Disc;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600L)
@@ -14,6 +19,14 @@ public class CartProductController extends AbstractController<CartProduct> {
 	public CartProductController() {
 		super(CartProduct.class);
 
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity addProductToCart(@RequestBody Disc entity) {
+		var c = new CartProduct();
+		c.setDisc(entity);
+		Filter<CartProduct> filter = new Filter<CartProduct>(c, clazz);
+		return saveCommand.execute(filter).buildResponse();
 	}
 
 
