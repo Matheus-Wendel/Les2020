da90e327d4f07cf8d9ae5470ae2e6495ba35754e
Matheus Wendel <matheus_wendel@outlook.com>
2021-04-12 17:38:30 -0300

    adicionado metodos para busca de stock

--- src/main/java/com/fatec/mogi/repository/StockRepository.java
+++ src/main/java/com/fatec/mogi/repository/StockRepository.java
@@ -1,11 +1,15 @@
 package com.fatec.mogi.repository;
 
-import com.fatec.mogi.model.domain.Stock;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fatec.mogi.model.domain.Stock;
+
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 
+	List<Stock> findByDiscIdOrderByPurchaceDateAsc(Integer id);	
+	List<Stock>  findByDiscIdOrderByCostPriceDesc(Integer id);	
 	
 	
 }
