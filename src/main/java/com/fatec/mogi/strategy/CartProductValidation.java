f12bf4abd68400e1417e53343804684cdaa7be68
Matheus Wendel <matheus_wendel@outlook.com>
2021-04-12 17:40:22 -0300

    fix validaçãopara adicionar items ao carrinho

--- src/main/java/com/fatec/mogi/strategy/CartProductValidation.java
+++ src/main/java/com/fatec/mogi/strategy/CartProductValidation.java
@@ -1,67 +1,66 @@
 package com.fatec.mogi.strategy;
 
 import java.util.Date;
+import java.util.List;
 
 import com.fatec.mogi.auth.AuthUtils;
 import com.fatec.mogi.model.domain.CartProduct;
 import com.fatec.mogi.model.domain.DomainEntity;
 import com.fatec.mogi.model.domain.Stock;
 import com.fatec.mogi.repository.DiscRepository;
+import com.fatec.mogi.repository.StockRepository;
 
 public class CartProductValidation implements IStrategy {
 
 	DiscRepository discRepository;
 
-	public CartProductValidation(DiscRepository discRepository) {
+	StockRepository stockRepository;
+
+	public CartProductValidation(DiscRepository discRepository, StockRepository stockRepository) {
 		this.discRepository = discRepository;
+		this.stockRepository = stockRepository;
 	}
 
 	@Override
 	public String process(DomainEntity entity) {
 		var cartProduct = (CartProduct) entity;
 		var sb = new StringBuilder();
+		cartProduct.setQuantity(1);
 
-		if (cartProduct.getQuantity() == 0) {
-			sb.append("Quantidade invalida");
-		}
 		if (cartProduct.getDisc() == null || cartProduct.getDisc().getId() == null) {
-			sb.append("Disco invalido");
+			sb.append("Disco invalido;;");
 		}
 		if (sb.length() > 0) {
 			return sb.toString();
 		}
 		var optionalDisc = discRepository.findById(cartProduct.getDisc().getId());
 		if (optionalDisc.isEmpty()) {
-			sb.append("Disco invalido");
+			sb.append("Disco invalido;;");
 			return sb.toString();
 		}
 		var disc = optionalDisc.get();
-		int discTotalStock = disc.getStock().stream().mapToInt(s -> s.getQuantity()).sum();
+		List<Stock> discStocks = stockRepository.findByDiscIdOrderByPurchaceDateAsc(disc.getId());
+		if (discStocks.isEmpty()) {
+			sb.append("Disco não possui nenhum estoque;;");
+		}
+		int discTotalStock = discStocks.stream().mapToInt(s -> s.getQuantity()).sum();
 		if (discTotalStock < cartProduct.getQuantity()) {
-			sb.append("Quantidade adicionada maior que o estoque do produto");
+			sb.append("Quantidade adicionada maior que o estoque do produto;;");
 		}
 		var clientCart = AuthUtils.getLoggedClient().getCart();
 		var present = clientCart.getCartProducts().stream().anyMatch(cp -> cp.getDisc().getId() == disc.getId());
 		if (present) {
-			sb.append("Disco ja esta no carrinho");
+			sb.append("Disco ja esta no carrinho;;");
 		}
 		if (sb.length() == 0) {
-			var discStocks = disc.getStock();
-			int productQuantity = cartProduct.getQuantity();
 			for (Stock stock : discStocks) {
-				if (productQuantity == 0) {
+				if (stock.getQuantity() >= 1) {
+					stock.setQuantity(stock.getQuantity() - 1);
 					break;
-				}
-				if (stock.getQuantity() >= productQuantity) {
-					stock.setQuantity(stock.getQuantity() - productQuantity);
-					break;
-				}
-				if (stock.getQuantity() < productQuantity && stock.getQuantity() > 0) {
-					productQuantity = productQuantity - stock.getQuantity();
-					stock.setQuantity(0);
 				}
 
 			}
+			disc.setStock(discStocks);
 			cartProduct.setCart(clientCart);
 			cartProduct.setDisc(disc);
 			cartProduct.setAddedDate(new Date());
