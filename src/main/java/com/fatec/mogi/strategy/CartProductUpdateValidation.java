21e8276d1e6d78eee2ad5d46f8be96ef2fbc6e3f
Matheus Wendel <matheus_wendel@outlook.com>
2021-04-12 17:39:09 -0300

    fix algoritmo para controle de estoque

--- src/main/java/com/fatec/mogi/strategy/CartProductUpdateValidation.java
+++ src/main/java/com/fatec/mogi/strategy/CartProductUpdateValidation.java
@@ -1,14 +1,16 @@
 package com.fatec.mogi.strategy;
 
-import java.util.Collections;
-import java.util.Comparator;
-
 import com.fatec.mogi.auth.AuthUtils;
 import com.fatec.mogi.model.domain.CartProduct;
+import com.fatec.mogi.model.domain.Client;
 import com.fatec.mogi.model.domain.DomainEntity;
 import com.fatec.mogi.model.domain.Stock;
 import com.fatec.mogi.repository.CartProductRepository;
+import com.fatec.mogi.repository.ClientRepository;
 import com.fatec.mogi.repository.DiscRepository;
+import com.fatec.mogi.repository.StockRepository;
+
+import jdk.jfr.Unsigned;
 
 public class CartProductUpdateValidation implements IStrategy {
 
@@ -16,9 +18,17 @@ public class CartProductUpdateValidation implements IStrategy {
 
 	CartProductRepository cartProductRepository;
 
-	public CartProductUpdateValidation(DiscRepository discRepository, CartProductRepository cartProductRepository) {
+	StockRepository stockRepository;
+
+	ClientRepository clientRepository;
+
+	public CartProductUpdateValidation(DiscRepository discRepository, CartProductRepository cartProductRepository,
+			StockRepository stockRepository, ClientRepository clientRepository) {
 		this.discRepository = discRepository;
 		this.cartProductRepository = cartProductRepository;
+		this.stockRepository = stockRepository;
+		this.clientRepository = clientRepository;
+
 	}
 
 	@Override
@@ -27,10 +37,10 @@ public class CartProductUpdateValidation implements IStrategy {
 		var sb = new StringBuilder();
 
 		if (cartProduct.getQuantity() == 0) {
-			sb.append("Quantidade invalida");
+			sb.append("Quantidade invalida;;");
 		}
 		if (cartProduct.getId() == null) {
-			sb.append("comando invalido");
+			sb.append("comando invalido;;");
 		}
 		if (sb.length() > 0) {
 			return sb.toString();
@@ -38,49 +48,53 @@ public class CartProductUpdateValidation implements IStrategy {
 
 		var optionalOldCartProduct = cartProductRepository.findById(cartProduct.getId());
 		if (optionalOldCartProduct.isEmpty()) {
-			sb.append("item não existe no carrinho");
+			sb.append("item não existe no carrinho;;");
 			return sb.toString();
 		}
+
 		var oldCartProduct = optionalOldCartProduct.get();
 		var disc = oldCartProduct.getDisc();
 		cartProduct.setDisc(oldCartProduct.getDisc());
 
-		int discTotalStock = disc.getStock().stream().mapToInt(s -> s.getQuantity()).sum();
-		
-		if (discTotalStock < cartProduct.getQuantity()&&(oldCartProduct.getQuantity()<cartProduct.getQuantity())) {
-			sb.append("Quantidade adicionada maior que o estoque do produto");
+		int discTotalStock = disc.getTotalStock();
+		var stockQuantitytoChange = oldCartProduct.getQuantity() - cartProduct.getQuantity();
+
+		if (discTotalStock < -stockQuantitytoChange && (stockQuantitytoChange < 0)) {
+			sb.append("Quantidade adicionada maior que o estoque do produto " + cartProduct.getDisc().getDescription()
+					+ ";;");
 		}
-		var clientCart = AuthUtils.getLoggedClient().getCart();
 
+		String userEmail = AuthUtils.getUserEmail();
+		Client client = clientRepository.findByUserEmail(userEmail);
 		if (sb.length() == 0) {
-			if (oldCartProduct.getQuantity() > cartProduct.getQuantity()) {
-				var stock = Collections.max(oldCartProduct.getDisc().getStock(),
-						Comparator.comparing(s -> s.getCostPrice()));
 
-				stock.setQuantity(stock.getQuantity() + (oldCartProduct.getQuantity() - cartProduct.getQuantity()));
-
-				cartProduct.setDisc(oldCartProduct.getDisc());
-			} else {
-
-				var discStocks = disc.getStock();
-				int productQuantity = cartProduct.getQuantity();
-				for (Stock stock : discStocks) {
+			if (stockQuantitytoChange < 0) {
+				var stockList = stockRepository.findByDiscIdOrderByCostPriceDesc(disc.getId());
+				int productQuantity = stockQuantitytoChange;
+				for (Stock stock : stockList) {
 					if (productQuantity == 0) {
 						break;
 					}
-					if (stock.getQuantity() >= productQuantity) {
-						stock.setQuantity(stock.getQuantity() - productQuantity);
+					if (stock.getQuantity() >= -productQuantity) {
+						stock.setQuantity(stock.getQuantity() + productQuantity);
 						break;
 					}
-					if (stock.getQuantity() < productQuantity && stock.getQuantity() > 0) {
-						productQuantity = productQuantity - stock.getQuantity();
+					if (stock.getQuantity() < -productQuantity && stock.getQuantity() > 0) {
+						productQuantity = productQuantity + stock.getQuantity();
 						stock.setQuantity(0);
 					}
 
 				}
+			} else if (stockQuantitytoChange > 0) {
+				var stockList = stockRepository.findByDiscIdOrderByCostPriceDesc(disc.getId());
+				var stock = stockList.get(0);
+				stock.setQuantity(stock.getQuantity() + stockQuantitytoChange);
+
+				cartProduct.setDisc(oldCartProduct.getDisc());
+				cartProduct.setCart(client.getCart());
 			}
-			cartProduct.setCart(clientCart);
 			cartProduct.setDisc(disc);
+			cartProduct.setCart(client.getCart());
 
 		}
 		return sb.toString();
