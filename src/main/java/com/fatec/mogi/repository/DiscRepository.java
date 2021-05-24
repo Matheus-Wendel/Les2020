67095b9622ce83f706ad5ceac159dd422d0d1d91
Matheus Wendel <matheus_wendel@outlook.com>
2021-05-23 22:58:09 -0300

    Add metodo para desativação

--- src/main/java/com/fatec/mogi/repository/DiscRepository.java
+++ src/main/java/com/fatec/mogi/repository/DiscRepository.java
@@ -3,6 +3,9 @@ package com.fatec.mogi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fatec.mogi.model.domain.Disc;
 
@@ -10,4 +13,8 @@ public interface DiscRepository extends JpaRepository<Disc, Integer> {
 
 	
 	List<Disc> findByNameContainingIgnoreCase(String name);
+	
+	@Modifying(clearAutomatically = true)
+	@Query("update Disc d set d.active=0 where d.id= :id")
+	public void deactivate(@Param(value = "id") Integer id); 
 }
