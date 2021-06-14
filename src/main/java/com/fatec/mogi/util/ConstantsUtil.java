40f1fe346f4b6e4a7620c95a147a9e8303d1d892
Matheus Wendel <matheus_wendel@outlook.com>
2021-06-13 22:24:15 -0300

    adicionado constate

--- src/main/java/com/fatec/mogi/util/ConstantsUtil.java
+++ src/main/java/com/fatec/mogi/util/ConstantsUtil.java
@@ -4,4 +4,5 @@ public class ConstantsUtil {
 	public static final String DATE_FORMAT = "yyyy-MM-dd";
 	public static final String CREDIT_CARD_EXPIRATION_DATE_FORMAT= "yyyy-MM";
 	public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd-HH:mm";
+	public static final String TIMESTAMP_INPUT_FORMAT = "yyyy-MM-dd'T'hh:mm";
 }
