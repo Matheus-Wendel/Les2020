1bff5557f9b50bf096d1313e9a81cd0c5f225168
Matheus Wendel <matheus_wendel@outlook.com>
2021-06-13 22:26:14 -0300

    adiciona metodo de busca

--- src/main/java/com/fatec/mogi/repository/PricingRepository.java
+++ src/main/java/com/fatec/mogi/repository/PricingRepository.java
@@ -1,11 +1,11 @@
 package com.fatec.mogi.repository;
 
-import com.fatec.mogi.model.domain.Pricing;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mogi.model.domain.Pricing;
 
 public interface PricingRepository extends JpaRepository<Pricing, Integer> {
 
-	
+	Pricing findBySaleId(Integer id);
 	
 }
